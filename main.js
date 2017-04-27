02fd5c5c86efc666f617d395215dc5ade4ca30fe
oscar gonzalez <oscargonzalez@oscars-MacBook-Pro.local>
2017-04-26 20:11:48 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+	$(".filter").on("click", function (e) {
+		e.preventDefault();
+
+		var button = $(this);
+		button.toggleClass("active");
+
+		$(".filter.active").each(function () {
+			var this_filter = $(this);
+			var filter = this_filter.data("filter");
+
+			if (filter === "all") {
+				$(".item").addClass("active");
+			} else {
+				$("." + filter).addClass("active");
+			}
+			if (filter === "reset") {
+				$(".item").removeClass("active");
+			}
+		});
+	});
+});
